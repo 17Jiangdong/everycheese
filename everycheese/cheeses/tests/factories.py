856f769ff57d80feebf2b7ff0bfa4b38990b6892
17Jiangdong <48679644+17Jiangdong@users.noreply.github.com>
2020-06-13 16:31:24 +0100

    Write tests for all cheese views

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -3,6 +3,8 @@ import factory
 import factory.fuzzy
 from ..models import Cheese
 
+from everycheese.users.tests.factories import UserFactory
+
 
 class CheeseFactory(factory.django.DjangoModelFactory):
 
@@ -11,6 +13,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     description = factory.Faker('paragraph', nb_sentences=3, variable_nb_sentences=True)
     firmness = factory.fuzzy.FuzzyChoice([x[0] for x in Cheese.Firmness.choices])
     country_of_origin = factory.Faker('country_code')
+    creator = factory.SubFactory(UserFactory)
 
     class Meta:
         model = Cheese
@@ -20,3 +23,9 @@ def test___str__():
     cheese = CheeseFactory()
     assert cheese.__str__() == cheese.name
     assert str(cheese) == cheese.name
+
+
+def test_get_absolute_url():
+    cheese = CheeseFactory()
+    url = cheese.get_absolute_url()
+    assert url == f'/cheeses/{cheese.slug}/'
