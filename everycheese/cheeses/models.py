ddac172c0ae4ed6d852e7a75349af744f94f5995
17Jiangdong <48679644+17Jiangdong@users.noreply.github.com>
2020-06-13 15:49:19 +0100

    Track and display the creator of each cheese

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.urls import reverse
+from django.conf import settings
 
 from autoslug import AutoSlugField
 from model_utils.models import TimeStampedModel
 
 
 class Cheese(TimeStampedModel):
-
-    def __str__(self):
-        return self.name
 
     class Firmness(models.TextChoices):
         UNSPECIFIED = "unspecified", "Unspecified"
@@ -23,6 +21,10 @@ class Cheese(TimeStampedModel):
     description = models.TextField("Description", blank=True)
     firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country of Origin", blank=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
 
     def get_absolute_url(self):
         """Return absolute URL to the Cheese Detail page."""
